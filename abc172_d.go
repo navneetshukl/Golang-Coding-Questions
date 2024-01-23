e1460ca9f57350589a9f84b4908198c3270ee1dd
Navneet Shukla <73691517+navneetshukl@users.noreply.github.com>
2024-01-23 19:38:19 +0530

    Create abc172_d.go

---
+++ abc172_d.go
@@ -0,0 +1,24 @@
+// https://atcoder.jp/contests/abc172/tasks/abc172_d
+.
+package main
+
+import "fmt"
+
+func main() {
+	var n int64
+	fmt.Scanln(&n)
+	var ans int64
+	ans = n * (n + 1) / 2
+	var i int64
+	var j int64
+	for i = 2; i <= n; i++ {
+		for j = i; j <= n; j = j + i {
+			ans += j
+		}
+	}
+	fmt.Println(ans)
+
+}
+
+
+
