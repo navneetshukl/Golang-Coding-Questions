16bb0bc8162b33020dfffd29839ca3f824729858
Navneet Shukla <73691517+navneetshukl@users.noreply.github.com>
2024-02-01 16:22:57 +0000

    arc-113/C

---
+++ arc-113/C.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func C() {
+	s := ""
+	fmt.Scan(&s)
+	var ans int64
+	n := len(s)
+	ans = 0
+
+	mp := map[string]int{}
+	mp[string(s[n-1])]++
+
+	for i := n - 2; i >= 1; i-- {
+		if string(s[i]) == string(s[i-1]) {
+			x := n - i - 1 - mp[string(s[i])]
+			ans += int64(x)
+
+			for i := 'a'; i <= 'z'; i++ {
+				mp[string(i)] = 0
+			}
+			mp[string(s[i])] = n - i
+
+		} else {
+			mp[string(s[i])]++
+		}
+	}
+	fmt.Println(ans)
+}
